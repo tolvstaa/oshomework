5e9d6dd214e16bcc5a2a6c1f5fdeb6dd124fce39
tolvstaa <tolvstaa@oregonstate.edu>
2015-10-20 06:35:22 -0700

    Started assn2 skeleton

--- assn2/tolvstaa.adventure.c
+++ assn2/tolvstaa.adventure.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define DIRPERMS S_IRWXU | S_IRGRP | S_IROTH
+
+void gen_roomfiles() {
+	// make files for rooms
+}
 
 int main() {
-	printf("Hello world");
+	char dirname[32];
+	sprintf(dirname, "tolvstaa.rooms.%d", getpid());
+
+	mkdir(dirname, DIRPERMS); // make directory
+	gen_roomfiles(); // generate all room files
+
+	printf("Hello world, process ID is %d\n", getpid());
+	
+	// for file in directory
+		// remove file
+	rmdir(dirname); // remove directory
 	return 0;
 }
