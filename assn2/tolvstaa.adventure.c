5f8ea89d397d5a3cd4f56ede254e1ffb37891c34
tolvstaa <tolvstaa@oregonstate.edu>
2015-11-03 11:16:12 -0800

    Implemented everything except user interaction and path tracking

--- assn2/tolvstaa.adventure.c
+++ assn2/tolvstaa.adventure.c
@@ -14,6 +14,13 @@
 #define NUM_ROOMS	7
 #define NAME_LEN	7
 #define TYPE_LEN	11
+
+#if TYPE_LEN > NAME_LEN
+	#define DATA_LEN TYPE_LEN
+#else
+	#define DATA_LEN NAME_LEN
+#endif
+
 #define CONN_MIN	3
 #define CONN_MAX	6
 
@@ -23,124 +30,170 @@
 const char NAMES[NUM_NAMES][NAME_LEN] = {"Room A","Room B","Room C",
 	"Room D","Room E","Room F","Room G","Room H","Room I","Room J"};
 
-
 typedef struct Room {
-	struct Room* connections[CONN_MAX];
+	struct Room* conns[CONN_MAX];
 	int cn_count;
 	char name[NAME_LEN];
 	char type[TYPE_LEN];
 } Room;
 
-typedef struct Graph { // TODO: refactor to not use a struct here
+typedef struct Graph {
 	Room data[NUM_ROOMS];
 } Graph;
-
-
-
-int rn_conns(int n) {
-	// returns random int, CONN_MIN-CONN_MAX inclusive
-	srand(time(NULL)+n);
-	return rand()%(CONN_MAX-CONN_MIN)+(CONN_MAX-(CONN_MAX-CONN_MIN));
-}
-
-
 
 void init_room(Room* r, const char name[NAME_LEN], int count, const char type[TYPE_LEN]) {
 	strcpy(r->name, name);
 	r->cn_count = count;
 	strcpy(r->type, type);
-	memset(r->connections, 0, CONN_MAX*sizeof(Room*));
+	memset(r->conns, 0, CONN_MAX*sizeof(Room*));
 }
 
+static inline int rn_conns(int n) { // returns random int, CONN_MIN-CONN_MAX inclusive
+	srand(time(NULL)+n);
+	return rand()%(CONN_MAX-CONN_MIN)+(CONN_MAX-(CONN_MAX-CONN_MIN));
+}
 
+static inline Room** next_null(Room** s) { // searches through n pointers to find the closest null pointer
+	int i;
+	for(i=0;i<CONN_MAX;i++) if(!s[i]) return s+i;
+	return NULL;
+}
+
+static inline int links_to(Room* src, Room* dest) {
+	int i;
+	for(i=0;i<CONN_MAX;i++) if(src->conns[i] == dest) return 1;
+	return 0;
+}
+
+static inline int count_conns(Room** s) {
+	int i;
+	for(i=0;i<CONN_MAX;i++) if(!s[i]) return i;
+	return CONN_MAX;
+}
+
+static inline Room* room_byname(Graph* g, const char* n) { // return address of room with name
+	int i;
+	for(i=0;i<NUM_ROOMS;i++) if(!strcmp(g->data[i].name, n)) return g->data+i;
+	return NULL;
+}
 
 void init_graph(Graph* g) {
-	int i,j,m;
+	int i,j,k,m;
 	
 	// randomize names
 	int write;
 	char r_names[NUM_ROOMS][NAME_LEN];				// random names
-	
 	for(i=0;i<NUM_ROOMS;i++) {						// for each slot in new array
 		srand(time(NULL)+i);
 		m = rand() % NUM_NAMES;						// pick random name from NAMES
-
 		write = 0;
-		while (!write && i != 0) {					// while name not inserted or errored out, skipping first
-			for (j=0;j<i;j++) {						// for existing names in new array
-				write = 1;
-				if(!strcmp(NAMES[m], r_names[j])) {	// if random name already exists
-					m=++m%NUM_NAMES;				// circular increment new room name
+		while (!write && i != 0)					// while name not inserted or errored out, skipping first
+			for (j=0;j<i;j++)						// for existing names in new array
+				if(write = 1, !strcmp(NAMES[m], r_names[j])) {	// if random name already exists
+					m=(m+1)%NUM_NAMES;				// circular increment new room name
 					write = 0;
 					break;							// start scanning existing names again
 				}
-			}
-		}											// success
 		strcpy(r_names[i], NAMES[m]);				// name DNE in new array yet, insert name
 	}
 	
-	// printer for r_names
-	/* for(i=0;i<NUM_ROOMS;i++)
-		printf("%s, ",r_names[i]);
-	printf("\b\b.\n"); */
-
 	// define rooms
-	init_room(&(g->data[0]), r_names[0], rn_conns(0), "START_ROOM");		// start room
+	init_room(g->data+0, r_names[0], rn_conns(0), "START_ROOM");		// start room
 	for(i=1;i<NUM_ROOMS-1;i++)
-		init_room(&(g->data[i]), r_names[i], rn_conns(i), "MID_ROOM");		// mid rooms
-	init_room(&(g->data[NUM_ROOMS-1]), r_names[NUM_ROOMS-1], rn_conns(NUM_ROOMS-1), "END_ROOM");	// end room
+		init_room(g->data+i, r_names[i], rn_conns(i), "MID_ROOM");		// mid rooms
+	init_room(g->data+NUM_ROOMS-1, r_names[NUM_ROOMS-1], rn_conns(NUM_ROOMS-1), "END_ROOM");	// end room
 
-	// TODO connect rooms
+	// connect rooms
+	for(i=0; i<NUM_ROOMS; i++)														// for each room
+		for(j=count_conns(g->data[i].conns); j<g->data[i].cn_count; j++)			// for each source slot
+			for(k=(i+NUM_ROOMS/2)%NUM_ROOMS,m=0; m<NUM_ROOMS; k=(k+1)%NUM_ROOMS,m++)// iterate destination circularly
+				// except itself. if dest has open conns, and not already conn'd,
+				if((i!=k)&&(count_conns(g->data[k].conns)<g->data[k].cn_count)&&(!links_to(g->data+i,g->data+k))){
+					g->data[i].conns[j] = g->data+k;								// connect room to dest
+					*(next_null(g->data[k].conns)) = g->data+i;						// connect dest to room
+					break;															// stop looking for dests
+				}
+	for(i=0;i<NUM_ROOMS;i++)
+		g->data[i].cn_count = count_conns(g->data[i].conns);						// correct any remaining nulls
 }
 
-void import_graph(Graph* g, char* dirname) {
-	DIR* d = opendir(dirname);
-	// TODO Read all data back into new graph and use graph for game logic
-	closedir(d);
+void data_import(Graph* g, const char* dirname, char mode) {
+	int i, talley;
+	char fpath_buf[FPATH_BUF_LEN], line_buf[16+DATA_LEN], field_buf[DATA_LEN];
+	char* csi, * cdi;
+	FILE* f;
+
+	for(i=0;i<NUM_ROOMS;i++) { // import conns
+		sprintf(fpath_buf, "%s/room%d", dirname, i);
+		f = fopen(fpath_buf, "r");
+		talley=0;
+
+		while(fgets(line_buf, sizeof(line_buf)-1, f)) {
+			memset(field_buf, 0, sizeof(field_buf));
+			for(csi=strstr(line_buf,": ")+2,cdi=field_buf;*csi;csi++,cdi++) // copy value to new buffer
+				*cdi = *csi;
+
+			if(mode == 'n') // name mode
+				if(strncmp(line_buf, "ROOM NAME", 9) == 0)
+					strcpy(g->data[i].name, field_buf); // copy name
+				else
+					strcpy(g->data[i].type, field_buf); // copy type
+			
+			else { // connection mode
+				if(strncmp(line_buf, "CONNECTION", 10) == 0) { // copy conns
+					*(next_null(g->data[i].conns)) = room_byname(g, field_buf);
+					talley++;
+				}
+				g->data[i].cn_count = talley; // set number of conns
+			}
+		}
+		fclose(f);
+	}
 }
 
-
+void import_graph(Graph* g, const char* dirname) {
+	int i;
+	for(i=0;i<NUM_ROOMS;i++) init_room(g->data+i,"",0,""); // init rooms to blank
+	data_import(g, dirname, 'n'); // import names and types
+	data_import(g, dirname, 'c'); // import conns and counts
+}
 
 void populate_file(char* fname, Room r) {
 	FILE* o = fopen(fname,"w");
-	fprintf(o,"ROOM NAME: %s\n",r.name);
-	
 	int i;
-	for (i=0;i<r.cn_count;i++) {
-		if(r.connections[i]) // FIXME
-			fprintf(o,"CONNECTION %d: %s\n",i+1,r.connections[i]->name);
+	fprintf(o,"ROOM NAME: %s\n",r.name);	// print name
+	for (i=0;i<r.cn_count;i++) {			// print connections
+		if(r.conns[i])
+			fprintf(o,"CONNECTION %d: %s\n",i+1,r.conns[i]->name);
 		else
-			fprintf(o,"ERROR: NULL CONNECTION\n");
+			fprintf(o,"CONNECTION %d: NULL\n",i+1);
 	}
-	
-	fprintf(o,"ROOM TYPE: %s\n", r.type);
+	fprintf(o,"ROOM TYPE: %s\n", r.type);	// print type
 	fclose(o);
 }
 
-void gen_roomfiles(char* dirname) {
+void gen_files(char* dirname) {
 	int i;
 	Graph g;
 	char buffer[FPATH_BUF_LEN];
 	
-	init_graph(&g);
+	init_graph(&g); // create a new graph
 	
-	for(i=0;i<NUM_ROOMS;i++) {
+	for(i=0;i<NUM_ROOMS;i++) { // print each room to a file
 		sprintf(buffer, "%s/room%d", dirname, i);
 		populate_file(buffer, g.data[i]);
 	}
 }
-
-
 
 int main(int argc, char** argv) {
 	char dirname[DIRNAME_LEN];
 	sprintf(dirname, "tolvstaa.rooms.%d", getpid());
 	
 	mkdir(dirname, DIRPERMS); // make directory
-	gen_roomfiles(dirname); // generate all room files
+	gen_files(dirname); // generate all room files
 	
-	// Graph g;
-	// import_graph(&g, dirname);
+	Graph g;
+	import_graph(&g, dirname);
 	
 	// TODO user interaction
 	
